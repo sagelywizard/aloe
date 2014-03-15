3315b30278fc27c17c9e315305d118427327f2ed
Benjamin Bastian <benjamin.bastian@gmail.com>
2014-03-14 18:22:42 -0700

    Add bad compaction algo. Should change later

--- src/aloe_file.erl
+++ src/aloe_file.erl
@@ -13,25 +13,40 @@
 -record(state, {
     tab,
     file,
-    key,
+    compact_file,
+    keypos,
     pending,
     pending_count,
-    fd
+    fd,
+    compacting,
+    cardinality,
+    compact_delta
 }).
 
 start_link(Filename, Options) ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [Filename, Options], []).
 
 init([Filename, Options]) ->
-    Key = case lists:keyfind(keypos, 1, Options) of
-        {keypos, Key0} -> Key0;
+    KeyPos = case lists:keyfind(keypos, 1, Options) of
+        {keypos, KeyPos0} -> KeyPos0;
         false -> none
     end,
     {ok, Fd} = file:open(Filename, [read, append, binary]),
     Tab = ets:new(list_to_atom(binary_to_list(Filename)), Options),
     file:position(Fd, 0),
     ok = read_file(Tab, Fd),
-    {ok, #state{file=Filename, key=Key, pending=[], pending_count=0, fd=Fd, tab=Tab}}.
+    CompactFile = Filename ++ ".aof",
+    {ok, #state{
+        file=Filename,
+        compact_file=CompactFile,
+        compacting=false,
+        keypos=KeyPos,
+        pending=[],
+        pending_count=0,
+        fd=Fd,
+        tab=Tab,
+        cardinality=hyper:new()
+    }}.
 
 read_file(Tab, Fd) ->
     case file:read(Fd, 4) of
@@ -52,10 +67,18 @@ handle_cast(_Msg, State) ->
     {noreply, State, 0}.
 
 handle_call({insert, Obj}, From, State) ->
-    #state{pending=Pending, pending_count=Count} = State,
+    #state{
+        pending=Pending,
+        pending_count=Count,
+        compact_delta=Delta,
+        cardinality=Card,
+        keypos=KeyPos
+    } = State,
     State0 = State#state{
         pending_count=Count+1,
-        pending=[{From, Obj}|Pending]
+        pending=[{From, Obj}|Pending],
+        compact_delta=Delta+1,
+        cardinality=hyper:insert(element(KeyPos, Obj), Card)
     },
     State1 = case State#state.pending_count of
         10 ->
@@ -63,14 +86,14 @@ handle_call({insert, Obj}, From, State) ->
         _ ->
             State0
     end,
-    {noreply, State1, 0};
+    {noreply, maybe_compact(State1), 0};
 
 handle_call({lookup, Obj}, _From, State) ->
     Tab = State#state.tab,
     {reply, ets:lookup(Tab, Obj), State, 0}.
 
 handle_info(timeout, State) ->
-    {noreply, commit(State)}.
+    {noreply, maybe_compact(commit(State))}.
 
 terminate(_Reason, _State) ->
     ok.
@@ -94,3 +117,35 @@ commit(State) ->
         gen_server:reply(From, true)
     end, Pending),
     State#state{pending=[], pending_count=0}.
+
+maybe_compact(State) ->
+    #state{cardinality=Card, compact_delta=Delta} = State,
+    case Delta / hyper:card(Card) > 3 of
+        Ratio when Ratio > 3 ->
+            compact(State);
+        _ ->
+            State
+    end.
+
+compact(State0) ->
+    State = State0#state{compacting=true},
+    #state{
+        tab=Tab,
+        compact_file=Filename,
+        file=AOF,
+        fd=AOFFD
+    } = State,
+    {ok, Fd} = file:open(Filename, [append, binary]),
+    {HLL, Count} = ets:foldl(fun(Obj, {HLL0, Count0}) ->
+        Bin = term_to_binary(Obj),
+        Size = iolist_size(Bin),
+        file:write(Fd, [<<Size:32/integer>>, Bin]),
+        {hyper:insert(term_to_binary(element(KeyPos, Obj)), HLL0), Count0+1}
+    end, {hyper:new(16), 0}, Tab),
+    file:sync(Fd),
+    ok = file:close(Fd),
+    ok = file:close(AOFFD),
+    ok = file:rename(Filename, AOF),
+    {ok, NewFd} = file:open(AOF, [read, append, binary]),
+    {ok, _} = file:position(NewFd, eof),
+    State#state{fd=NewFd}.
